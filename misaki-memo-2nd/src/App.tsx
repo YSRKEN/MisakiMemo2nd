23281cf7434bb6e12f3db742f4ed6efeb296678b
YSRKEN <ysr.ken@gmail.com>
2019-09-26 16:09:32 +0900

    [add] React Bootstrapをインストールし、サンプルフォームが構築できることを確かめた
    シェルで「yarn add react-bootstrap bootstrap 」と入力し、その後「typesync」で型情報を引っ張ってくる

--- misaki-memo-2nd/src/App.tsx
+++ misaki-memo-2nd/src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col, Form } from 'react-bootstrap';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Container>
+    <Row>
+      <Col className="m-3">
+        <h1 className="text-center">美咲メモ 2nd</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={12} md={6} className="my-3 mx-auto">
+        <Form className="border p-3">
+          <Form.Group controlId="idolName">
+            <Form.Label>アイドルの名前</Form.Label>
+            <Form.Control type="text" placeholder="名前の一部(かな文字もOK)" />
+          </Form.Group>
+          <Form.Group controlId="idolType">
+            <Form.Label>アイドルの属性</Form.Label>
+            <Form.Control as="select">
+              <option className="test">Princess</option>
+              <option>Fairy</option>
+              <option>Angel</option>
+            </Form.Control>
+          </Form.Group>
+          <Button className="w-100">テスト</Button>
+        </Form>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default App;
